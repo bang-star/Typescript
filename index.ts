cb6f7b24cc08419cc188144542b13b949852ea54
bang-star <63120360+bang-star@users.noreply.github.com>
2023-07-31 23:38:50 +0900

    Revert "Method"

--- index.ts
+++ index.ts
@@ -1,20 +1,44 @@
-class Person {
-    name: string;
-    age: number;
-    height: number = 10;
+let foo = 3;        // number
+let fuz = "string";  // string
 
-    public setOlder(n: number) {
-        this.age += n;
-    }
+// === Best Common type === //
+let bar = [1, 2, "string"]  // => (string | number)[]
+
+// === Contextual typing === //
+[1, 2, 3, 4].forEach((element) => { console.log(element)} );
+
+let fifo = function (param) {
+    console.log(param);
 }
 
-const person = new Person();
-person.name = 'BBB';
-person.age = 25;            // X
-person.height = 180;        // X
+enum FrontEnd {
+    HTML,
+    CSS,
+    JavaScript
+}
 
-console.log(person.name, person.age, person.height);
 
-person.setOlder(3);         // 28
+console.log(FrontEnd.HTML);         // 0
+console.log(FrontEnd.JavaScript);   // 2
+console.log(FrontEnd.CSS);          // 1
 
-console.log(person.name, person.age, person.height);
+enum FrontEnd2 {
+    HTML,
+    CSS = 7,
+    JavaScript
+}
+
+console.log(FrontEnd2.HTML)         // 0
+console.log(FrontEnd2.CSS)          // 7
+console.log(FrontEnd2.JavaScript)   // 8
+
+
+enum FrontSkill {
+    HTML = "HTML",
+    CSS = 1,
+    JavaScript = "JavaScript"
+}
+
+console.log(FrontSkill.HTML)              // "HTML"
+console.log(FrontSkill.CSS)               // 1
+console.log(FrontSkill.JavaScript)        // "1"
