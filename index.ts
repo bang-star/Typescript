b8d60c1da37027ff502b885952e76c556e8b887a
bang-star <bang77060986@gmail.com>
2023-08-01 23:29:40 +0900

    #38 - 제네릭 제약조건

--- index.ts
+++ index.ts
@@ -1,54 +1,31 @@
-let foo: Array<number> = [1, 2, 3];
-
 /**
- * Union 타입을 이용한 방법
- * => 파라미터와 반환 타입이 동일하게 설정되어 리턴타입이 하나로 고정되는 것을 기대할 수 없음.
- * number => number | string
- * string => number | string
- * 이로 인해 원하지 않는 방식으로 동작할 수 있게 됨.
+ * 제네릭 제약조건을 사용하지 않은 경우
+ *
+ * name이라는 프로퍼티가 없을 수 있으므로 name을 출력하기 위해서는 조건문을 통해 name 프로퍼티 검사가 이루어져야 한다.
  */
-function returnInput1(input: number | string) {
-    return input;
+function logName<T>(input: T) {
+ console.log(input.name);
 }
 
 /**
- * any 타입
- * @param input
- * any로 설정하면 반환 값도 any가 되어 적절하지 않음.
+ * 제네릭 제약조건을 사용하는 경우
  */
-function returnInput2(input: any) {
-    return input;
+interface WithName {
+    name: string;
+}
+
+function logName2<T extends WithName>(input: T) {
+    console.log(input.name);
 }
 
 /**
- * 제네릭을 이용한 방법
- * @param input
- * 제네릭을 이용하면 모든 타입을 받을 수 있으면서 파라미터와 반환 타입을 동일하게 일치시켜줄 수 있다.
+ * 타입 매개변수의 활용
  */
-function returnInput3<T>(input: T) {
-    return input;
+function logPropertyValue<T, K extends keyof T>(obj: T, key: K) {
+    console.log(obj[key]);
 }
 
-console.log(typeof returnInput3(3))          // number
-console.log(typeof returnInput3("ABC"))      // string
-console.log(typeof returnInput3([1, 2, 3]))  // array
+const foo = { a: 1, b: 2};
 
-console.log(returnInput3<number>(3));        // 명시적으로 사용할 수 있음.
-
-
-interface GenericItems<T> {
-    items: T[];
-}
-
-function genericFunction(args: GenericItems<number>) {
-    console.log(args.items);
-}
-
-class GenericClass<T> {
-    someProperty: T;
-}
-
-let generic = new GenericClass<number>();
-
-generic.someProperty = "asd";   // X
-generic.someProperty = 333;     // O
+logPropertyValue(foo, "a"); // O
+logPropertyValue(foo, "d"); // X
