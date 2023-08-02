0c83d73f1860980536bf4d936097e26060735e8b
bang-star <63120360+bang-star@users.noreply.github.com>
2023-08-03 00:30:52 +0900

    d.ts (#77)
    * #72 - Namespaced (#75)
    
    * #73 - Module (#76)
    
    * #74 - d.ts

---
+++ 6_3.ts
@@ -0,0 +1,3 @@
+let a: A;
+
+let c: C;
--- index.ts
+++ index.ts
@@ -1,34 +1,9 @@
 /**
- * Assertion
+ * Module
  */
 
-// ========== EX 1 ========== //
-let someString: string = "10";
+import {foo, baz, someFunc} from "./d";
 
-function logInput(input: "10") {
-    console.log(input);
-}
-
-logInput(someString as "10");
-
-// ========== EX 2 ========== //
-
-interface Person {
-    name: string;
-    age: number;
-}
-
-interface Developer extends Person {
-    field: string;
-}
-
-function getName(person : Person) {
-    return person.name;
-}
-
-getName({name: "Kim", age: 25, field: "FE"} as Person);
-
-// ========== EX 3 ========== //
-
-const errCase: string = "error!" as number;                 // X
-const errCase: string = "error!" as unknown as number;      // O
+console.log(foo)
+console.log(baz)
+console.log(someFunc)
