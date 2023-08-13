8a12602bb52038c5859b32a48c01d4e5ba774fff
bang-star <bang77060986@gmail.com>
2023-08-13 22:40:46 +0900

    #80 - Partial

--- index.ts
+++ index.ts
@@ -1,9 +1,28 @@
 /**
- * Module
+ * Partial
  */
 
-import {foo, baz, someFunc} from "./a";
+type Developer = {
+    name?: string
+    age?: number
+    field: string
+}
 
-console.log(foo)
-console.log(baz)
-console.log(someFunc)
+// OK
+let somePerson1: Developer = {
+    name: '김개발',
+    age: 25,
+    field: 'FE'
+};
+
+// X(field 프로퍼티는 필수)
+let somePerson2: Developer = {
+    name: '김개발',
+    age: 25,
+};
+
+// O
+let somePerson3: Partial<Developer> = {
+    name: '김개발',
+    age: 25,
+};
