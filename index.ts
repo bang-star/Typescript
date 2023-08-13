20c690ac943682b27481df2a959d2a3815a5d065
bang-star <63120360+bang-star@users.noreply.github.com>
2023-08-13 22:54:44 +0900

    #83 - Omit (#90)

--- index.ts
+++ index.ts
@@ -1,22 +1,28 @@
 /**
- * Pick
+ * Omit
  */
 
 type Developer = {
     name: string
     age: number
-    field?: string
+    field: string
 }
 
-// X -> 특정 필드만 가지고 있어야 함.
-let somePerson1: Pick<Developer, "name"> = {
+let somePerson1: Developer = {
     name: '김개발',
     age: 25,
     field: 'FE'
 };
 
-// OK
-let somePerson2: Pick<Developer, "name" | "age"> = {
+// X
+let somePerson2: Omit<Developer, "name" | "age"> = {
     name: '김개발',
     age: 25,
+    field: 'FE'
 };
+
+// O
+let somePerson3: Omit<Developer, "name"> = {
+    age: 25,
+    field: 'FE'
+}
